f0c4aec678154b7a4c9448964633345ebd82ce0b
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-08-24 22:49:13 -0400

    Health Check added

--- app/main/tv/codely/app/mooc/controller/health_check/HealthCheckGetController.java
+++ app/main/tv/codely/app/mooc/controller/health_check/HealthCheckGetController.java
@@ -1,14 +1,13 @@
 package tv.codely.app.mooc.controller.health_check;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 @RestController
-public final class HealthCheckGetController {
-
-    @RequestMapping("/health-check")
+public class HealthCheckGetController {
+    @GetMapping("/health-check")
     public HashMap<String, String> index() {
         HashMap<String, String> status = new HashMap<>();
         status.put("status", "ok");

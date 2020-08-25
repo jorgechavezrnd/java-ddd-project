33d8eaaf5a31600a91e04d031b4a3971e2a35d09
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-08-24 23:13:22 -0400

    Test for Health Check added, and HealthCheckGetController modfied to final class

--- app/main/tv/codely/app/mooc/controller/health_check/HealthCheckGetController.java
+++ app/main/tv/codely/app/mooc/controller/health_check/HealthCheckGetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
-public class HealthCheckGetController {
+public final class HealthCheckGetController {
     @GetMapping("/health-check")
     public HashMap<String, String> index() {
         HashMap<String, String> status = new HashMap<>();

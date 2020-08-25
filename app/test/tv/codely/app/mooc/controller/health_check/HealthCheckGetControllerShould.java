362ff6c90397539ea2a30707a242c0a732526da8
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-08-24 23:26:55 -0400

    Test for HealthCheckGetController renamed

--- app/test/tv/codely/app/mooc/controller/health_check/HealthCheckGetControllerShould.java
+++ app/test/tv/codely/app/mooc/controller/health_check/HealthCheckGetControllerShould.java
@@ -5,7 +5,7 @@ import tv.codely.app.mooc.controller.RequestTestCase;
 
 final class HealthCheckGetControllerShould extends RequestTestCase {
     @Test
-    void check_health_check_is_working() throws Exception {
+    void check_the_app_is_working_ok() throws Exception {
         assertResponse("/health-check", 200, "{'status':'ok'}");
     }
 }

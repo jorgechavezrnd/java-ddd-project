fa79f910a9a0992c3f681e0bfd701d164f0e5f52
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-08-25 00:06:30 -0400

    CoursePutController skeleton with test added

---
+++ app/main/tv/codely/app/mooc/controller/courses/CoursesPutController.java
@@ -0,0 +1,37 @@
+package tv.codely.app.mooc.controller.courses;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public final class CoursesPutController {
+    @PutMapping("/courses/{id}")
+    public ResponseEntity create(@PathVariable String id, @RequestBody Request request) {
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+}
+
+final class Request {
+    private String name;
+    private String duration;
+
+    String name() {
+        return name;
+    }
+
+    String duration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/test/tv/codely/app/mooc/controller/courses/CoursesPutControllerShould.java
@@ -0,0 +1,16 @@
+package tv.codely.app.mooc.controller.courses;
+
+import org.junit.jupiter.api.Test;
+import tv.codely.app.mooc.controller.RequestTestCase;
+
+public final class CoursesPutControllerShould extends RequestTestCase {
+    @Test
+    void create_a_valid_course() throws Exception {
+        assertRequestWithBody(
+            "PUT",
+            "/courses/1aab45ba-3c7a-4344-8936-78466eca77fa",
+            "{\"name\": \"The best course\", \"duration\": \"5 hours\"}",
+            201
+        );
+    }
+}

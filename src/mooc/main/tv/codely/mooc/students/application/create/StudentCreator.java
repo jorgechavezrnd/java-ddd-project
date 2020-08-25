f6cf5c10e91274bcb7f9ca0d433e3ca513345349
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-08-25 10:56:17 -0400

    Student class added in domain for Mooc context

--- src/mooc/main/tv/codely/mooc/students/application/create/StudentCreator.java
+++ src/mooc/main/tv/codely/mooc/students/application/create/StudentCreator.java
@@ -1,10 +1,11 @@
 package tv.codely.mooc.students.application.create;
 
+import tv.codely.mooc.students.domain.Student;
 import tv.codely.shared.domain.Service;
 
 @Service
 public final class StudentCreator {
     public void create(String id, String name, String email) {
-
+        Student student = new Student(id, name, email);
     }
 }

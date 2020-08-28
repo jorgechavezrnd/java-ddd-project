fc0a08af599bb8788003fe77dc2dd0318ab53c0e
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-08-28 02:24:17 -0400

    StudentsModuleUniTest base class created and now StudentCreatorShould exend of this class, also CreateStudentRequestMother added

--- src/mooc/test/tv/codely/mooc/students/application/create/StudentCreatorShould.java
+++ src/mooc/test/tv/codely/mooc/students/application/create/StudentCreatorShould.java
@@ -1,32 +1,29 @@
 package tv.codely.mooc.students.application.create;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import tv.codely.mooc.students.domain.*;
+import tv.codely.mooc.students.StudentsModuleUnitTestCase;
+import tv.codely.mooc.students.domain.Student;
+import tv.codely.mooc.students.domain.StudentMother;
 
-import static org.mockito.Mockito.*;
+final class StudentCreatorShould extends StudentsModuleUnitTestCase {
+    private StudentCreator creator;
 
-final class StudentCreatorShould {
+    @BeforeEach
+    protected void setUp() {
+        super.setUp();
+
+        creator = new StudentCreator(repository);
+    }
+
     @Test
     void save_a_valid_student() {
-        StudentRepository repository = mock(StudentRepository.class);
-        StudentCreator    creator    = new StudentCreator(repository);
+        CreateStudentRequest request = CreateStudentRequestMother.random();
 
-        CreateStudentRequest request = new CreateStudentRequest(
-            "1aab45ba-3c7a-4344-8936-78466eca77fa",
-            "name",
-            "surname",
-            "email"
-        );
-
-        Student student = new Student(
-            new StudentId(request.id()),
-            new StudentName(request.name()),
-            new StudentSurname(request.surname()),
-            new StudentEmail(request.email())
-        );
+        Student student = StudentMother.fromRequest(request);
 
         creator.create(request);
 
-        verify(repository, atLeastOnce()).save(student);
+        shouldHaveSaved(student);
     }
 }

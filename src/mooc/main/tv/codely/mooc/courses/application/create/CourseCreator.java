1067f59bed10fe3df8d469e81f62bf4f73c8478a
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-04 14:15:17 -0400

    CourseCreator and UnitTestCase modified for working now with event bus. CoursesCreatedDomainEventMother added too, tests for CourseCreatedShould working, but CoursesPutControllerShould not working because there is not a bean, or implementation, for EventBus interface

--- src/mooc/main/tv/codely/mooc/courses/application/create/CourseCreator.java
+++ src/mooc/main/tv/codely/mooc/courses/application/create/CourseCreator.java
@@ -2,22 +2,26 @@ package tv.codely.mooc.courses.application.create;
 
 import tv.codely.mooc.courses.domain.*;
 import tv.codely.shared.domain.Service;
+import tv.codely.shared.domain.bus.event.EventBus;
 
 @Service
 public final class CourseCreator {
     private final CourseRepository repository;
+    private final EventBus eventBus;
 
-    public CourseCreator(CourseRepository repository) {
+    public CourseCreator(CourseRepository repository, EventBus eventBus) {
         this.repository = repository;
+        this.eventBus   = eventBus;
     }
 
     public void create(CreateCourseRequest request) {
-        Course course = new Course(
-            new CourseId(request.id()),
-            new CourseName(request.name()),
-            new CourseDuration(request.duration())
-        );
+        CourseId       id = new CourseId(request.id());
+        CourseName     name = new CourseName(request.name());
+        CourseDuration duration = new CourseDuration(request.duration());
+
+        Course course = Course.create(id, name, duration);
 
         repository.save(course);
+        eventBus.publish(course.pullDomainEvents());
     }
 }
--- src/shared/test/tv/codely/shared/infrastructure/UnitTestCase.java
+++ src/shared/test/tv/codely/shared/infrastructure/UnitTestCase.java
@@ -1,4 +1,34 @@
 package tv.codely.shared.infrastructure;
 
+import org.junit.jupiter.api.BeforeEach;
+import tv.codely.shared.domain.UuidGenerator;
+import tv.codely.shared.domain.bus.event.DomainEvent;
+import tv.codely.shared.domain.bus.event.EventBus;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
 public abstract class UnitTestCase {
+    protected EventBus      eventBus;
+    protected UuidGenerator uuidGenerator;
+
+    @BeforeEach
+    protected void setUp() {
+        eventBus = mock(EventBus.class);
+        uuidGenerator = mock(UuidGenerator.class);
+    }
+
+    public void shouldHavePublished(List<DomainEvent<?>> domainEvents) {
+        verify(eventBus, atLeastOnce()).publish(domainEvents);
+    }
+
+    public void shouldHavePublished(DomainEvent<?> domainEvent) {
+        shouldHavePublished(Collections.singletonList(domainEvent));
+    }
+
+    public void shouldGenerateUuid(String uuid) {
+        when(uuidGenerator.generate()).thenReturn(uuid);
+    }
 }

6183594612c7747ed32543c9a85dfbdbc15d9e59
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-04 12:24:39 -0400

    Refactor for MySqlCourseRepository implementation done and tests working

--- src/mooc/main/tv/codely/mooc/courses/infrastructure/persistence/MySqlCourseRepository.java
+++ src/mooc/main/tv/codely/mooc/courses/infrastructure/persistence/MySqlCourseRepository.java
@@ -5,26 +5,23 @@ import tv.codely.mooc.courses.domain.Course;
 import tv.codely.mooc.courses.domain.CourseId;
 import tv.codely.mooc.courses.domain.CourseRepository;
 import tv.codely.shared.domain.Service;
+import tv.codely.shared.infrastructure.hibernate.HibernateRepository;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
-public class MySqlCourseRepository implements CourseRepository {
-    private final SessionFactory sessionFactory;
-
+public class MySqlCourseRepository extends HibernateRepository<Course> implements CourseRepository {
     public MySqlCourseRepository(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+        super(sessionFactory, Course.class);
     }
 
     @Override
-    @Transactional
     public void save(Course course) {
-        sessionFactory.getCurrentSession().save(course);
+        persist(course);
     }
 
     @Override
     public Optional<Course> search(CourseId id) {
-        return Optional.ofNullable(sessionFactory.getCurrentSession().find(Course.class, id));
+        return byId(id);
     }
 }

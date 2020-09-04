b057cba6b458cc936be56be109f5d616875668bb
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-04 10:46:24 -0400

    SQL Query for create students table added, and Student model with Value Objects created

--- src/mooc/main/resources/database/mooc.sql
+++ src/mooc/main/resources/database/mooc.sql
@@ -5,6 +5,14 @@ CREATE TABLE `courses` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `students` (
+  `id` CHAR(36) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `surname` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 CREATE TABLE `steps` (
     `id` CHAR(36) NOT NULL,
     `title` VARCHAR(155) NOT NULL,
---
+++ src/mooc/main/tv/codely/mooc/students/domain/StudentRepository.java
@@ -0,0 +1,4 @@
+package tv.codely.mooc.students.domain;
+
+public interface StudentRepository {
+}

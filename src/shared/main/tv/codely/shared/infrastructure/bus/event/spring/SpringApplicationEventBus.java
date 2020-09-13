a1fe1a34f29b8fe762fad2631c08103eefc06b7c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-12 22:15:46 -0400

    SpringApplicationEventBus changed to new folder

--- src/shared/main/tv/codely/shared/infrastructure/bus/event/spring/SpringApplicationEventBus.java
+++ src/shared/main/tv/codely/shared/infrastructure/bus/event/spring/SpringApplicationEventBus.java
@@ -1,4 +1,4 @@
-package tv.codely.shared.infrastructure.bus.event;
+package tv.codely.shared.infrastructure.bus.event.spring;
 
 import org.springframework.context.ApplicationEventPublisher;
 import tv.codely.shared.domain.Service;

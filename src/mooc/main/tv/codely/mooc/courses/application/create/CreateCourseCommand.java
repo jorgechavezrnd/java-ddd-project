7fb58d38a231d522942bc6c773938b4561754f00
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-16 23:15:52 -0400

    Equals method modified in CreateCourseCommand

--- src/mooc/main/tv/codely/mooc/courses/application/create/CreateCourseCommand.java
+++ src/mooc/main/tv/codely/mooc/courses/application/create/CreateCourseCommand.java
@@ -30,7 +30,7 @@ public final class CreateCourseCommand implements Command {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof CreateCourseCommand)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         CreateCourseCommand that = (CreateCourseCommand) o;
         return id.equals(that.id) &&
             name.equals(that.name) &&

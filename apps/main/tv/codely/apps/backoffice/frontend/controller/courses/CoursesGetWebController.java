9c681d8cc506a6652d1eae20a5ac853b0434b6ec
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-17 21:38:11 -0400

    Courses counter value showed in Backoffice fronted, in controller, the catch section is temporal

---
+++ apps/main/tv/codely/apps/backoffice/frontend/controller/courses/CoursesGetWebController.java
@@ -0,0 +1,40 @@
+package tv.codely.apps.backoffice.frontend.controller.courses;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import tv.codely.mooc.courses_counter.application.find.CoursesCounterResponse;
+import tv.codely.mooc.courses_counter.application.find.FindCoursesCounterQuery;
+import tv.codely.shared.domain.bus.query.QueryBus;
+import tv.codely.shared.domain.bus.query.QueryHandlerExecutionError;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+@Controller
+public final class CoursesGetWebController {
+    private final QueryBus bus;
+
+    public CoursesGetWebController(QueryBus bus) {
+        this.bus = bus;
+    }
+
+    @GetMapping("/courses")
+    public ModelAndView index() throws QueryHandlerExecutionError {
+         try {
+             CoursesCounterResponse counterResponse = bus.ask(new FindCoursesCounterQuery());
+
+             return new ModelAndView("pages/courses/courses", new HashMap<String, Serializable>() {{
+                 put("title", "Courses");
+                 put("description", "Courses CodelyTV - Backoffice");
+                 put("courses_counter", counterResponse.total());
+             }});
+         } catch (Exception e) {
+             return new ModelAndView("pages/courses/courses", new HashMap<String, Serializable>() {{
+                 put("title", "Courses");
+                 put("description", "Courses CodelyTV - Backoffice");
+                 put("courses_counter", 0);
+             }});
+         }
+    }
+}

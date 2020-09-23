4d7bb2f380ab1d6def1d9c45d1997fb3a9585939
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-22 23:59:59 -0400

    Object mother for Backoffice course criteria added, also tests for search by criteria with hibernate in backoffice courses added, all tests working

--- src/backoffice/test/tv/codely/backoffice/courses/infrastructure/persistence/MySqlBackofficeCourseRepositoryShould.java
+++ src/backoffice/test/tv/codely/backoffice/courses/infrastructure/persistence/MySqlBackofficeCourseRepositoryShould.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import tv.codely.backoffice.BackofficeContextInfrastructureTestCase;
 import tv.codely.backoffice.courses.domain.BackofficeCourse;
+import tv.codely.backoffice.courses.domain.BackofficeCourseCriteriaMother;
 import tv.codely.backoffice.courses.domain.BackofficeCourseMother;
 import tv.codely.backoffice.courses.domain.BackofficeCourseRepository;
+import tv.codely.shared.domain.criteria.Criteria;
 
 import javax.transaction.Transactional;
 
@@ -35,5 +37,25 @@ class MySqlBackofficeCourseRepositoryShould extends BackofficeContextInfrastruct
         repository.save(anotherCourse);
 
         assertThat(Arrays.asList(course, anotherCourse), containsInAnyOrder(repository.searchAll().toArray()));
+    }
+
+    @Test
+    void search_courses_using_a_criteria() {
+        BackofficeCourse matchingCourse        = BackofficeCourseMother.create("DDD en Java", "3 days");
+        BackofficeCourse anotherMatchingCourse = BackofficeCourseMother.create("DDD en TypeScript", "2.5 days");
+        BackofficeCourse intellijCourse        = BackofficeCourseMother.create("Exprimiendo Intellij", "48 hours");
+        BackofficeCourse cobolCourse           = BackofficeCourseMother.create("DDD en Cobol", "5 years");
+
+        Criteria criteria = BackofficeCourseCriteriaMother.nameAndDurationContains("DDD", "days");
+
+        repository.save(matchingCourse);
+        repository.save(anotherMatchingCourse);
+        repository.save(intellijCourse);
+        repository.save(cobolCourse);
+
+        assertThat(
+            Arrays.asList(matchingCourse, anotherMatchingCourse),
+            containsInAnyOrder(repository.matching(criteria).toArray())
+        );
     }
 }

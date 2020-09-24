1322b4a62e3e2d489ad4cd59cc8925ba59f9fa89
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-23 22:30:00 -0400

    Implementation for basci auth HTTP added for Backoffice backend

--- apps/main/tv/codely/apps/backoffice/backend/config/BackofficeBackendServerConfiguration.java
+++ apps/main/tv/codely/apps/backoffice/backend/config/BackofficeBackendServerConfiguration.java
@@ -1,13 +1,19 @@
 package tv.codely.apps.backoffice.backend.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tv.codely.shared.domain.bus.command.CommandBus;
+import tv.codely.apps.backoffice.backend.middleware.BasicHttpAuthMiddleware;
 
 @Configuration
 public class BackofficeBackendServerConfiguration {
-    private final CommandBus bus;
+    @Bean
+    public FilterRegistrationBean<BasicHttpAuthMiddleware> basicHttpAuthMiddleware() {
+        FilterRegistrationBean<BasicHttpAuthMiddleware> registrationBean = new FilterRegistrationBean<>();
 
-    public BackofficeBackendServerConfiguration(CommandBus bus) {
-        this.bus = bus;
+        registrationBean.setFilter(new BasicHttpAuthMiddleware());
+        registrationBean.addUrlPatterns("/health-check");
+
+        return registrationBean;
     }
 }

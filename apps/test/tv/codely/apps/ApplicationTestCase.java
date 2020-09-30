d32aa54b7dd3fb51e2e6dc11751b744195a7ccf4
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-09-30 12:22:40 -0400

    Tests for HealtchCheckGetController in Backoffice backend controllers, added

--- apps/test/tv/codely/apps/ApplicationTestCase.java
+++ apps/test/tv/codely/apps/ApplicationTestCase.java
@@ -4,14 +4,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import tv.codely.shared.domain.bus.event.DomainEvent;
 import tv.codely.shared.domain.bus.event.EventBus;
-
-import javax.transaction.Transactional;
 
 import java.util.Arrays;
 
@@ -24,14 +23,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 public abstract class ApplicationTestCase {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private EventBus eventBus;
 
-    public void assertResponse(
+    protected void assertResponse(
         String endpoint,
         Integer expectedStatusCode,
         String expectedResponse
@@ -46,7 +44,23 @@ public abstract class ApplicationTestCase {
             .andExpect(response);
     }
 
-    public void assertRequestWithBody(
+    protected void assertResponse(
+        String endpoint,
+        Integer expectedStatusCode,
+        String expectedResponse,
+        HttpHeaders headers
+    ) throws Exception {
+        ResultMatcher response = expectedResponse.isEmpty()
+            ? content().string("")
+            : content().json(expectedResponse);
+
+        mockMvc
+            .perform(get(endpoint).headers(headers))
+            .andExpect(status().is(expectedStatusCode))
+            .andExpect(response);
+    }
+
+    protected void assertRequestWithBody(
         String method,
         String endpoint,
         String body,
@@ -54,6 +68,17 @@ public abstract class ApplicationTestCase {
     ) throws Exception {
         mockMvc
             .perform(request(HttpMethod.valueOf(method), endpoint).content(body).contentType(APPLICATION_JSON))
+            .andExpect(status().is(expectedStatusCode))
+            .andExpect(content().string(""));
+    }
+
+    protected void assertRequest(
+        String method,
+        String endpoint,
+        Integer expectedStatusCode
+    ) throws Exception {
+        mockMvc
+            .perform(request(HttpMethod.valueOf(method), endpoint))
             .andExpect(status().is(expectedStatusCode))
             .andExpect(content().string(""));
     }
